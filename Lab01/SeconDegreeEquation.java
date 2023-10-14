22f37d7261909b90bcaf33b126f65b0190ec226e
unknown <huydogiaccac@gmail.com>
2023-10-14 13:03:35 +0700

    Seventh commit

---
+++ Lab01/SeconDegreeEquation.java
@@ -0,0 +1,41 @@
+//Do Gia Huy
+//20215060
+import java.util.Scanner;
+//java.lang không cần import
+public class SeconDegreeEquation {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Nhập các hệ số từ người dùng
+        System.out.print("Nhập hệ số a: ");
+        double a = sc.nextDouble();
+        System.out.print("Nhập hệ số b: ");
+        double b = sc.nextDouble();
+        System.out.print("Nhập hệ số c: ");
+        double c = sc.nextDouble();
+
+        // Tính Delta
+        double delta = b * b - 4 * a * c;
+
+        if (delta > 0) {
+            // Hai nghiệm thực
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Nghiệm của phương trình là:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        } else if (delta == 0) {
+            // Một nghiệm kép
+            double x = -b / (2 * a);
+            System.out.println("Phương trình có nghiệm kép:");
+            System.out.println("x = " + x);
+        } else {
+            // Nghiệm phức
+            double re = -b / (2 * a);
+            double im = Math.sqrt(-delta) / (2 * a);
+            System.out.println("Phương trình có nghiệm phức:");
+            System.out.println("x1 = " + re + " + " + im + "i");
+            System.out.println("x2 = " + re + " - " + im + "i");
+        }
+    }
+}
